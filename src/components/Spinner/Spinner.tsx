85df5abfcfc66c40eb4b66a676cdd933e2f299b1
SYED175 <syedismailgp@gmail.com>
2024-07-18 15:53:37 +0530

    Update styles for Spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,13 +1,14 @@
 const Spinner = () => {
   return (
-    <div>
+    <div className="container d-flex flex-column align-items-center m-4">
       <div
-        className="spinner-border sd-spinner"
+        className="spinner-border mb-3"
         style={{ width: "5rem", height: "5rem" }}
         role="status"
       >
         <span className="visually-hidden">...</span>
       </div>
+      <p>It may take upto 50 seconds or more if strapi is inactive..</p>
     </div>
   );
 };
